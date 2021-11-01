3d89604968fe947641d8ed8115c226fb4b38ff5b
apoablue <apoablue@gmail.com>
2021-11-01 13:40:24 +0000

    github pages set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const app_config = {
   devtool: 'none',
   mode: 'development',
   entry: './client/index.js',
+  plugins: [
+    new CleanWebpackPlugin(),
+    // new HtmlWebpackPlugin({
+    //   title: 'Mundungus',
+    //   // template: path.join(__dirname, './public/index.html')
+    // }),
+  ],
   output: {
-    path: path.join(__dirname, './public/dist'),
-    filename: 'bundle.js',
+    path: path.join(__dirname, './build'),
+    // filename: 'bundle.js',
   },
   devServer: {
-    contentBase: path.join(__dirname, './public/dist'),
+    // contentBase: path.join(__dirname, './public/dist'),
     compress: true,
     port: 9000
   },
